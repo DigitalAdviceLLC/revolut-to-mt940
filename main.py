123b50f5e7e32a696771fcb65f6bda9973d185fb
Sankar NRI <san@grainpot.com>
2021-03-01 21:23:35 +0530

    remove arguments

--- main.py
+++ main.py
@@ -9,23 +9,12 @@ def main():
         prog='oddity-revolut-to-mt940',
         description='Convert Revolut CSV-files to MT940 format.')
 
-    parser.add_argument('--in',
-                        dest='input_file',
-                        help='path to Revolut csv-file',
-                        required=True)
-
-    parser.add_argument('--account-iban',
-                        dest='account_iban',
-                        help='Revolut account IBAN',
-                        required=True)
-
-    parser.add_argument('--out',
-                        dest='output_file',
-                        help='path to MT940 output path',
-                        required=True)
-
-    args = parser.parse_args()
-
+    #REPLACE IBAN IN THE FOLLOWING LINE
+    args.account_iban = 'REPLACE_REAL_IBAN_HERE'
+    
+    args.input_file = 'input.csv'
+    args.output_file = 'output.mt940'
+    
     reader = RevolutCsvReader(args.input_file)
 
     with Mt940Writer(args.output_file, args.account_iban) as writer:
